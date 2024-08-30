93e310223d5a2a9309ed08bae2e45897faa0efc5
HieuChiChau <chauchihieu2003@gmail.com>
2024-08-30 10:09:37 +0700

    ngu2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.5.3', // Thay bằng địa chỉ IP cục bộ của bạn
+    host: '172.26.151.184', // Thay bằng địa chỉ IP cục bộ của bạn
     port: 5173, // Hoặc bất kỳ cổng nào bạn muốn sử dụng
   },
 });
