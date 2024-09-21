db4f5972e46fea237033083a1615ca2f7a35e24e
HoangLeAnhTuan <87897985+HoangLeAnhTuan@users.noreply.github.com>
2024-09-22 01:37:22 +0700

    Update api.js with deploy AI

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.215:5000', // Thay đổi baseURL cho phù hợp với API của bạn
-    timeout: 10000, // Thay đổi thời gian chờ (timeout) nếu cần
+    //baseURL: 'http://192.168.1.215:5000', // Thay đổi baseURL cho phù hợp với API của bạn
+    baseURL: 'https://danagreenai.serveo.net', 
+    timeout: 5000, // Thay đổi thời gian chờ (timeout) nếu cần
 });
 
 export default api;
